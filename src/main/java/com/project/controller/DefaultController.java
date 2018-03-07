b84990d52d5ab7610f16f6aa18dcc39f18eb0105
Dimon1234 <wap.mmj@mail.ru>
2018-03-08 00:44:15 +0300

    Service layer with logging added; added delete controller

--- src/main/java/com/project/controller/DefaultController.java
+++ src/main/java/com/project/controller/DefaultController.java
@@ -1,27 +1,41 @@
 package com.project.controller;
 
-import com.project.repository.MealRepository;
+import com.project.service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
 
-    private final MealRepository mealRepository;
+
+    private final MealService mealService;
 
     @Autowired
-    public DefaultController(MealRepository mealRepository) {
-        this.mealRepository = mealRepository;
+    public DefaultController(MealService mealService) {
+        this.mealService = mealService;
     }
+
 
     @GetMapping("/")
     public ModelAndView home(ModelAndView model) {
-        model.addObject("meals",  mealRepository.findAll());
+        model.addObject("meals",  mealService.getAll());
         model.setViewName("list");
         return model;
     }
+
+    @GetMapping("/delete")
+    public ModelAndView delete(ModelAndView model, @RequestParam long id)
+    {
+        mealService.deleteMeal(id);
+        model.addObject("meals", mealService.getAll());
+        model.setViewName("list");
+        return model;
+    }
+
+
 
 
 }
---
+++ src/main/java/com/project/service/MealService.java
@@ -0,0 +1,46 @@
+package com.project.service;
+
+
+import com.project.model.Meal;
+import com.project.model.MealWithExceed;
+import com.project.repository.MealRepository;
+import com.project.util.MealsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Service
+public class MealService {
+
+    @Autowired
+    private MealRepository repository;
+
+    private Logger LOG = LoggerFactory.getLogger(MealService.class);
+
+    public void saveMeal(Meal meal) {
+        repository.save(meal);
+        LOG.debug("meal " + meal + " saved successfully");
+    }
+
+    public void deleteMeal(long id) {
+        repository.delete(id);
+        LOG.debug("meal with id " + id + " deleted successfully");
+    }
+
+    public void edit(Meal meal) {
+        repository.save(meal);
+        LOG.debug("meal "+meal+" edited successfully");
+    }
+
+    public List<Meal> getAll() {
+        return repository.findAll();
+    }
+
+    public List<MealWithExceed> getListMealWithExceed(int caloriesPerDay) {
+        return MealsUtil.getFilteredWithExceeded(repository.findAll(), LocalTime.MIN, LocalTime.MAX, caloriesPerDay);
+    }
+}
